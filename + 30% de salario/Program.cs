d779f4a06ef2ea68fb620379a34cb671f45b7f68
Tamashi19 <29raul19@gmail.com>
2021-05-26 17:20:51 -0300

    estruturar condicionais

---
+++ + 30% de salario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace __30__de_salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira seu salario:");
+            int salario = int.Parse(Console.ReadLine());
+
+            if (salario>500)
+            {
+                Console.WriteLine("Desculpe, seu salario não atende os requisitos para o aumento");
+                
+            }
+
+            else
+            {
+                Console.WriteLine("Seu salario atende os requisitos para o aumento");
+                int por100= salario*100+(30*salario*100)/100;
+                int aumento= por100/100;
+                Console.WriteLine("Seu salario agora é de: " +aumento);
+            }
+            
+        }
+    }
+}
---
+++ idade em semanas/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace idade_em_semanas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coloque seu ano de nascimento:");
+            int anoDeNascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Coloque seu mes de nascimento:");
+            int mesDeNascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Coloque seu dia de nascimento:");
+            int diaDeNascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Coloque o ano de hoje:");
+            int anoDeHoje = int.Parse(Console.ReadLine());
+            Console.WriteLine("Coloque o mes de hoje:");
+            int mesDehoje = int.Parse(Console.ReadLine());
+            Console.WriteLine("Coloque o dia de hoje:");
+            int diaDeHoje = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Seu ano de nascimento é: " + anoDeNascimento);
+            Console.WriteLine("Seu mes de nascimento é: " + mesDeNascimento);
+            Console.WriteLine("Seu dia de nascimento é: " + diaDeNascimento);
+        
+
+
+            // NÃO FEZ ANIVERSARIO
+            
+            if (mesDeNascimento>mesDehoje )
+            {
+              int ano = anoDeHoje - anoDeNascimento -1;  
+
+              Console.WriteLine("Você tem: " + ano + " anos" );
+             
+              int semana = ano*52177457;
+
+                   Console.WriteLine("Você tem: " + semana + " semanas" );
+
+           
+            }
+
+            // JA FEZ ANIVERSARIO
+
+            else
+            {
+                int ano = anoDeHoje - anoDeNascimento ;
+                
+                 Console.WriteLine("Você tem: " + ano + " anos" );
+                 
+                  int semana = ano*52177457;
+
+                   Console.WriteLine("Você tem: " + semana + " semanas" );
+
+                  
+                  
+            }
+
+
+
+        }
+    }
+}
